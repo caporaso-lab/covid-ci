e78230f8868dd3858fe9b8b6ef75257dea9982dd
Evan Bolyen <ebolyen@gmail.com>
2020-07-08 15:29:51 -0700

    WIP: dummy pipeline

---
+++ bin/import.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os
+
+cwd = os.cwd()
+
+input_dir = os.path.join(cwd, 'input_dir')
+output = os.path.join(cwd, 'output_dir', 'file.txt')
+
+print(os.path.listdir(input_dir))
+
+with open(output, 'w') as fh:
+    fh.write("hello world")
+
+
