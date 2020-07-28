704cefb7153f4b295403f955bb5d19b5ce2f5aa0
Evan Bolyen <ebolyen@gmail.com>
2020-07-28 11:35:13 -0700

    Import VCF mask

--- bin/transfer.py
+++ bin/transfer.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
 import os
+import time
 
 import common
 
-input_file = os.path.join('input', os.listdir('input')[0])
-file_name = os.path.basename(input_file)
+if 'INPUT_FILE' in os.environ:
+    input_file = os.path.join('input', os.environ['INPUT_FILE'])
+    file_name = str(int(time.time())) + '_' + os.environ['INPUT_FILE']
+else:
+    input_file = os.path.join('input', os.listdir('input')[0])
+    file_name = os.path.basename(input_file)
 
 output_path = os.path.join(os.environ['EXTERNAL_DIR'], file_name)
 
