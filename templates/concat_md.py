d23e78e35dcb64430bfc9ec06acc63e5adf40317
Evan Bolyen <ebolyen@gmail.com>
2020-07-29 07:58:43 -0700

    Fix metadata, add label seqs

--- templates/concat_md.py
+++ templates/concat_md.py
@@ -12,10 +12,10 @@ dfs = [pd.read_csv(f, sep='\t') for f in metadata_files]
 merged_df = pd.concat(dfs)
 
 path = os.path.join('{{ output }}', 'metadata-' + str(uuid.uuid4()) + '.tsv')
-merged_df.to_csv(path, sep='\t', index=False)
 merged_df = merged_df[[
     'id', 'gisaid_epi_isl', 'region', 'country', 'division', 'location',
     'combined_location', 'date', 'submitting_lab']]
+merged_df.to_csv(path, sep='\t', index=False)
 
 with open(os.path.join(os.getcwd(), 'manifest.json'), 'w') as fh:
     fh.write(json.dumps({'result': path}))
